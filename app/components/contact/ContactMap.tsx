1422f24dda378b99cbc247d75f424fd7a1796fec
masud <masud.official@gmail.com>
2025-09-14 22:21:15 +0600

    fix contact

--- app/components/contact/ContactMap.tsx
+++ app/components/contact/ContactMap.tsx
@@ -4,12 +4,23 @@ export default function ContactMap() {
   return (
     <section className="overflow-hidden mt-10 md:mt-[130px] w-full">
       <div className="relative w-full h-[240px] sm:h-[450px] md:h-[540px] lg:h-[700px] xl:h-[700px] mx-auto xl:rounded-lg overflow-hidden">
-        <iframe
-          className="map-iframe w-full h-full block"
-          src="https://www.google.com/maps/d/embed?mid=1hPZlx7IzAPJ3ss2IAz8v2xVInwgYJ6Y&ehbc=2E312F"
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
+        {(() => {
+          const address =
+            "House - 69 , Road - 06 , Block - A , Mirpur - 12 , Dhaka - 1216";
+          const mapSrc = `https://www.google.com/maps?q=${encodeURIComponent(
+            address
+          )}&output=embed`;
+
+          return (
+            <iframe
+              className="map-iframe w-full h-full block"
+              src={mapSrc}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              allowFullScreen
+            />
+          );
+        })()}
       </div>
     </section>
   );
